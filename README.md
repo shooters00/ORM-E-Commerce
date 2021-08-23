7801f7c4f74b238b2f1df10ac263ea8c8befc727
shooters00 <shooters0000@gmail.com>
2021-08-23 08:56:45 -0400

    Readme and Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ORM-E-Commerce
+
+<a href="" rel="nofollow"><img src="https://img.shields.io/badge/license-none-blue.svg" alt="None" title="None" style="max-width:100%;"></a>
+# E-COMMERCE BACK END
+
+## Description
+This project builds the back end for an e-commerce site by modifying starter code. It configures a working Express.js API to use Sequelize to interact with a MySQL database.
+
+#### Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+Use schema.sql file in the db folder to create the database.  Seed the data using the files by running 'node seeds'.  Run 'npm start' to start the server.  
+
+## Usage
+Utilize insomnia to perform searches, creates, updates, and deletes.
+
+## License
+This project is covered under the None license. 
+
+## Contributing
+N/A
+
+## Tests
+N/A
+
+## Questions
+My GitHub: https://github.com/shooters00
+
+For additional questions, contact me at: shooters0000@gmail.com
+
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -87,47 +87,7 @@ router.put('/:id', async (req, res) => {
   } catch (err) {
       res.status(500).json(err);
     };
-    
-/*
-    Tag.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((tag) => {
-        // find all associated tags from ProductTag
-        return ProductTag.findAll({ where: { tag_id: req.params.id } });
-      })
-      .then((productTags) => {
-        // get list of current tag_ids
-        const productTagIds = productTags.map(({ product_id }) => product_id);
-        // create filtered list of new tag_ids
-        const newProductTags = req.body.tagIds
-          .filter((product_id) => !productTagIds.includes(product_id))
-          .map((product_id) => {
-            return {
-              tag_id: req.params.id,
-              product_id,
-            };
-          });
-        // figure out which ones to remove
-        const productTagsToRemove = productTags
-          .filter(({ product_id }) => !req.body.productIds.includes(product_id))
-          .map(({ id }) => id);
-  
-        // run both actions
-        return Promise.all([
-          ProductTag.destroy({ where: { id: productTagsToRemove } }),
-          ProductTag.bulkCreate(newProductTags),
-        ]);
-      })
-      .then((updatedProductTags) => res.json(updatedProductTags))
-      .catch((err) => {
-        // console.log(err);
-        res.status(400).json(err);
-      });
 
-*/
 });
 
 router.delete('/:id', async (req, res) => {
